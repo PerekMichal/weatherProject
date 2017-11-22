d5aced8196049fdb40453a225301d4da14d0f1fa
Perek Michal <perekmichal93@gmail.com>
2017-11-22 13:27:07 +0100

    first commit

---
+++ src/main/java/pl/perekmichal/weatherProject/WeatherProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.perekmichal.weatherProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherProjectApplication.class, args);
+	}
+}
